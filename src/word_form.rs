13304f36e19e21a057483ed33010135b0020ba46
hawks <yobett@hxr.com>
2025-04-07 12:14:41 +0800

    json

--- src/word_form.rs
+++ src/word_form.rs
@@ -1,3 +1,37 @@
+use serde_json::{Value};
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+
+// struct Entry {
+//     word: String,
+//     base_form: String,
+// }
+
+fn build_base_forms(file_name: &str) {
+    //     'word', 'baseForm', 'baseForms', 'examType',
+    //         'phonetics', 'pronAllUk', 'pronAllUs', 'simple',
+    //         'forms', 'categories', 'phrases'
+
+    let file = File::open(file_name).unwrap();
+    let mut lines = io::BufReader::new(file).lines();
+    lines.next();
+    let it = lines.next();
+    let fl = it.unwrap().unwrap();
+    println!("fl: {}", fl);
+    let v: Value = serde_json::from_str(&fl).unwrap();
+    println!("v: {:?}", v);
+    if let Value::Object(o) = v {
+        println!("word: {:?}", o.get("word").or(None).unwrap());
+        println!("word2: {:?}", o.get("word2"));
+    }
+}
+
+#[test]
+fn test_build_froms() {
+    build_base_forms(r#"C:\ws\node\cla-data\bson\dict-comp.jsonl"#);
+}
+
 fn guest_base_forms(word: &str) -> Vec<String> {
     let mut forms: Vec<String> = Vec::new();
     let len = word.len();
