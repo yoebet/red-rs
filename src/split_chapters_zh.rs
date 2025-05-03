7217a1a2303a6b0d3b289ca8c287fe6f51c8cf43
hawks <yobett@hxr.com>
2025-05-03 12:46:34 +0800

    split_chapters_zh

--- src/split_chapters_zh.rs
+++ src/split_chapters_zh.rs
@@ -1,14 +1,14 @@
-use std::fs::File;
-use std::fs;
-use std::path::{Path, PathBuf};
 use crate::SResult;
+use regex::Regex;
+use std::collections::HashMap;
+use std::fmt::{Debug, Display};
+use std::fs::File;
 use std::io::prelude::*;
+use std::path::PathBuf;
 
 fn split_chapters(path: &str) -> SResult {
     let sp = PathBuf::from(path);
-    let fnn = sp.file_name().unwrap().to_str().unwrap();
-    let bn = fnn.find("_").unwrap();
-    let base_name = &fnn[0..bn];
+    let base_name = "chapter";
     let base_dir = sp.parent().unwrap();
     if let Ok(lines) = crate::read_lines(path) {
         let mut current_ch = "".to_string();
@@ -21,10 +21,14 @@ fn split_chapters(path: &str) -> SResult {
             let mut f = File::create(np).unwrap();
             let _ = f.write_all(content.as_bytes());
         };
+        let re = Regex::new(r"^第([一二三四五六七八九十]{1,3})回\b").unwrap();
 
         for line in lines.map_while(Result::ok) {
-            if line.starts_with("CHAPTER ") {
-                let chapter = line.trim_start_matches("CHAPTER ").trim();
+            let m = re.captures(&line);
+            if let Some(c) = m {
+                let cz = c.get(1).unwrap();
+                let cn = zh2n2(cz.as_str());
+                let chapter = &cn.to_string()[..];
                 if chapter != current_ch {
                     if current_ch != "" && !ch_lines.is_empty() {
                         save_chapter(&ch_lines.join("\r\n"), &current_ch);
@@ -45,39 +49,81 @@ fn split_chapters(path: &str) -> SResult {
 }
 
 #[test]
-fn test_fbn() -> SResult {
-    // let ss=["aa", "_", "bb", ".txt"].join("\\");
-    let path = r"C:\ws\node\voc\wt\chamber\chamber_6-10.txt";
-    let sp = PathBuf::from(path);
-    let fnn = sp.file_name().unwrap().to_str().unwrap();
-    println!("{:?}", fnn);
-    let bn = fnn.find("_").unwrap();
-    println!("{:?}", &fnn[0..bn]);
-    Ok(())
-}
-
-#[test]
 fn test_split_chapters() -> SResult {
-    split_chapters(r"C:\ws\node\voc\wt\chamber\chamber_11-15.txt")
+    split_chapters(r"C:\ws\node\voc\wt\hlm-rw\红楼梦 舒芜序.txt")
 }
 
 #[test]
-fn test_split_dir_chapters() -> SResult {
-    let dir = r"C:\ws\node\voc\wt\stone";
-    let d = Path::new(dir);
-    println!("dir {:?}", d.file_name());
-    for fe in fs::read_dir(d)?.into_iter() {
-        let f = fe?;
-        if !f.metadata()?.is_file() {
-            continue;
-        }
-        let path = &f.path();
-        let pss = path.to_str().unwrap();
-        let hb = pss.find("-");
-        if let Some(_) = hb {
-            println!("{}", pss);
-            let _ = split_chapters(pss);
-        }
+fn test_match_nums() {
+    // let ns="一二三四五六七八九十";
+    let re = Regex::new(r"^第([一二三四五六七八九十]{1,3})回\s+").unwrap();
+    println!("{:?}", re);
+    let m = re.captures("第五十一回 　薛小妹新编怀古诗 胡庸医乱用虎狼药");
+    if let Some(c) = m {
+        println!("{:?}", c);
+        let cz = c.get(1).unwrap();
+        println!("{:?}", cz);
+        let cn = zh2n2(cz.as_str());
+        println!("chapter_{:?}", cn);
+    } else {
+        println!("{:?}", "...");
     }
-    Ok(())
+}
+
+fn zh2n(zn: &str) -> usize {
+    let zs = "0一二三四五六七八九十";
+    let mut cs = zn.chars();
+    let znl = zn.chars().count();
+
+    let z1 = cs.next().unwrap();
+    let (n1, _) = zs.chars().enumerate().find(|(_, c)| *c == z1).unwrap();
+
+    if znl == 1 {
+        return n1;
+    }
+    if znl == 2 {
+        if n1 == 10 {
+            let z2 = cs.next().unwrap();
+            let (n2, _) = zs.chars().enumerate().find(|(_, c)| *c == z2).unwrap();
+            return n1 + n2;
+        }
+        return n1 * 10;
+    }
+    cs.next();
+    let z3 = cs.next().unwrap();
+    let (n3, _) = zs.chars().enumerate().find(|(_, c)| *c == z3).unwrap();
+    n1 * 10 + n3
+}
+
+fn zh2n2(zn: &str) -> i32 {
+    let zn_vec: Vec<char> = zn.chars().collect();
+    let mut zhm: HashMap<char, i32> = HashMap::new();
+    for (i, v) in "一二三四五六七八九十".chars().enumerate() {
+        zhm.insert(v, i as i32 + 1);
+    }
+    let v = zhm.get(&zn_vec[0]).unwrap();
+    if zn_vec.len() == 1 {
+        return *v;
+    }
+    if zn_vec.len() == 2 {
+        if *v == 10 {
+            let v1 = zhm.get(&zn_vec[1]).unwrap();
+            return v + v1;
+        }
+        return v * 10;
+    }
+    let v2 = zhm.get(&zn_vec[2]).unwrap();
+    v * 10 + v2
+}
+
+#[test]
+fn test_zhh_nums() {
+    println!("{:?}", zh2n("五"));
+    println!("{:?}", zh2n("五十"));
+    println!("{:?}", zh2n("五十一"));
+    println!("{:?}", zh2n("十一"));
+    println!("{:?}", zh2n2("五"));
+    println!("{:?}", zh2n2("五十"));
+    println!("{:?}", zh2n2("五十一"));
+    println!("{:?}", zh2n2("十一"));
 }
