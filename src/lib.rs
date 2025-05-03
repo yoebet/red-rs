fac4c9ed581adfa264ba992afd4a993a777e9ea2
hawks <yobett@hxr.com>
2025-05-03 09:17:24 +0800

    split_chapters_zh

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 mod word_form;
 mod split_chapters_en;
+mod split_chapters_zh;
 mod rename_files;
 
 use std::io;
 use std::io::BufRead;
 use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
 
 type SResult = Result<(), io::Error>;
 
@@ -14,3 +17,10 @@ pub mod ma {
     }
 }
 
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
